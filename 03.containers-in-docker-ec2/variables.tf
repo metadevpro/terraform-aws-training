e4cdfcda88b6323cd835f73e1671805720eedb5d
Pedro J. Molina <pjmolina@gmail.com>
2021-03-17 21:09:51 +0100

    default author to acme

--- 03.containers-in-docker-ec2/variables.tf
+++ 03.containers-in-docker-ec2/variables.tf
@@ -13,7 +13,7 @@
 variable "author" {
   description = "Name of the operator. Used as a prefix to avoid name collision on resources."
   type        = string
-  default     = "pjmolina"
+  default     = "acme"
 }
 
 variable "region" {
